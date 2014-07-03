f13428bcc16a73cd1c77c971ffb00ace75ad53c1
Brian Tanabe <btanabe2@gmail.com>
2014-07-02 20:35:29 -0500

    Removing Spring stuff.  I've never used it for command line applications, didn't realize it was so difficult to disable logging.
    Stubbed out improperly failing Application integration test.  Working on the console redirect.

--- src/main/java/com/tanabe/rs/application/Application.java
+++ src/main/java/com/tanabe/rs/application/Application.java
@@ -1,19 +1,11 @@
 package com.tanabe.rs.application;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-
 /**
  * Created by Brian on 7/2/2014.
  */
-public class Application implements CommandLineRunner {
+public class Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
+        System.out.println("I'M RUNNING");
     }
 }
--- src/test/java/com/tanabe/test/ApplicationIntegrationTest.java
+++ src/test/java/com/tanabe/test/ApplicationIntegrationTest.java
@@ -1,15 +1,33 @@
 package com.tanabe.test;
 
+import com.tanabe.rs.application.Application;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Brian on 7/2/2014.
  */
 public class ApplicationIntegrationTest {
+    private static PrintStream consolePrintStream;
+    private static PrintStream newPrintStream;
+
+    @BeforeClass
+    public static void setUpConsolePrintingToString(){
+        // Set up redirects here:
+    }
+
 
     @Test
     public void testOneMillionTwoHundredThirtyFourThousandFiveHundredSixtySevenDollarsEightyNineCents(){
-        String[] args = new String[] { "1234567.89"};
 
+
+        String[] args = new String[] { "1234567.89"};
+        Application.main(args);
+
+        assertEquals("One million two hundred thirty-four thousand five hundred sixty-seven and 89/100 dollars", "");
     }
 }
