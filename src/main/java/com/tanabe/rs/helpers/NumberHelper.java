3ac973ae2fa96c7302c5a555074a8815b44a2870
Brian Tanabe <btanabe2@gmail.com>
2014-07-02 20:03:36 -0500

    Refactored/cleaned up NumberHelper

--- src/main/java/com/tanabe/rs/helpers/NumberHelper.java
+++ src/main/java/com/tanabe/rs/helpers/NumberHelper.java
@@ -6,15 +6,17 @@ import java.math.BigDecimal;
  * Created by Brian on 7/2/2014.
  */
 public class NumberHelper {
+    private static final int ZERO_DECIMAL_PLACES = 0;
+    private static final int TWO_DECIMAL_PLACES = 2;
 
     public static BigDecimal getIntegerPart(double number){
-        return BigDecimal.valueOf(number).setScale(0, BigDecimal.ROUND_FLOOR);
+        return BigDecimal.valueOf(number).setScale(ZERO_DECIMAL_PLACES, BigDecimal.ROUND_FLOOR);
     }
 
     public static int getFractionalPartTruncatedAfterTwoDecimalPlacesAsInteger(double number){
         BigDecimal numberAsBigDecimal = BigDecimal.valueOf(number);
-        numberAsBigDecimal.setScale(2, BigDecimal.ROUND_FLOOR);
-        numberAsBigDecimal = numberAsBigDecimal.subtract(BigDecimal.valueOf(number).setScale(0, BigDecimal.ROUND_FLOOR));
+        numberAsBigDecimal.setScale(TWO_DECIMAL_PLACES, BigDecimal.ROUND_FLOOR);
+        numberAsBigDecimal = numberAsBigDecimal.subtract(BigDecimal.valueOf(number).setScale(ZERO_DECIMAL_PLACES, BigDecimal.ROUND_FLOOR));
         return numberAsBigDecimal.multiply(BigDecimal.valueOf(100)).intValue();
     }
 }
