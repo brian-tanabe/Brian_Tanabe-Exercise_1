1ed4b5b3e038c489e6a1e2ae6079e5770acff887
Brian Tanabe <btanabe2@gmail.com>
2014-07-02 19:42:52 -0500

    Stubbed NumberToEnglishTranslator and implemented failing test

---
+++ src/main/java/com/tanabe/rs/translator/NumberToEnglishTranslator.java
@@ -0,0 +1,13 @@
+package com.tanabe.rs.translator;
+
+/**
+ * Created by Brian on 7/2/2014.
+ */
+public class NumberToEnglishTranslator {
+
+
+    public static String translateNumber(double number){
+
+        return "";
+    }
+}
---
+++ src/test/java/com/tanabe/test/NumberTranslatorTest.java
@@ -0,0 +1,17 @@
+package com.tanabe.test;
+
+import org.junit.Test;
+
+import static com.tanabe.rs.translator.NumberToEnglishTranslator.translateNumber;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Brian on 7/2/2014.
+ */
+public class NumberTranslatorTest {
+
+    @Test
+    public void testTranslateTwoThousandFiveHundredTwentyThreeAndFourDollars(){
+        assertEquals("Two thousand five hundred twenty-three and 04/100 dollars", translateNumber(2523.04));
+    }
+}
