03b758d362b21505e895c9446bc7e665f82025ab
Brian Tanabe <btanabe2@gmail.com>
2014-07-02 21:38:51 -0500

    Console redirect for ApplicationIntegrationTest working properly.  Tests fail.

--- src/test/java/com/tanabe/test/ApplicationIntegrationTest.java
+++ src/test/java/com/tanabe/test/ApplicationIntegrationTest.java
@@ -1,9 +1,11 @@
 package com.tanabe.test;
 
 import com.tanabe.rs.application.Application;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
@@ -12,22 +14,28 @@ import static org.junit.Assert.assertEquals;
  * Created by Brian on 7/2/2014.
  */
 public class ApplicationIntegrationTest {
+    private static PrintStream oldPrintStream;
     private static PrintStream consolePrintStream;
-    private static PrintStream newPrintStream;
+    private static ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
     @BeforeClass
     public static void setUpConsolePrintingToString(){
-        // Set up redirects here:
+        consolePrintStream = new PrintStream(byteArrayOutputStream);
+        oldPrintStream = System.out;
+
+        System.setOut(consolePrintStream);
     }
 
+    @AfterClass
+    public static void tearDown(){
+        System.setOut(oldPrintStream);
+    }
 
     @Test
     public void testOneMillionTwoHundredThirtyFourThousandFiveHundredSixtySevenDollarsEightyNineCents(){
-
-
         String[] args = new String[] { "1234567.89"};
         Application.main(args);
 
-        assertEquals("One million two hundred thirty-four thousand five hundred sixty-seven and 89/100 dollars", "");
+        assertEquals("One million two hundred thirty-four thousand five hundred sixty-seven and 89/100 dollars", byteArrayOutputStream.toString());
     }
 }
